7bddb1706fdccbdc7300a5222fe3e87b4e675593
Nathan Osman <admin@quickmediasolutions.com>
2012-05-08 17:16:11 -0700

    Moved a lot of the code into separate files.

--- src/file-webp.c
+++ src/file-webp.c
@@ -1,6 +1,6 @@
 /*=======================================================================
               WebP load / save plugin for the GIMP
-                 Copyright 2011 - Nathan Osman
+                 Copyright 2012 - Nathan Osman
 
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
@@ -16,19 +16,14 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 =======================================================================*/
 
-// Standard includes
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-
-// Gimp and WebP headers
 #include <libgimp/gimp.h>
 #include <libgimp/gimpui.h>
 
-#include <webp/decode.h>
-#include <webp/encode.h>
+#include "read-webp.h"
+#include "write-webp.h"
+#include "export-dialog.h"
 
-// Procedure names
 const char LOAD_PROCEDURE[] = "file-webp-load";
 const char SAVE_PROCEDURE[] = "file-webp-save";
 const char BINARY_NAME[]    = "file-webp";
@@ -36,11 +31,6 @@ const char BINARY_NAME[]    = "file-webp";
 // Predeclare our entrypoints
 void query();
 void run(const gchar *, gint, const GimpParam *, gint *, GimpParam **);
-
-int read_webp(const gchar *);
-int write_webp(const gchar *, gint, float);
-
-int save_dialog(float *);
 
 // Declare our plugin entry points
 GimpPlugInInfo PLUG_IN_INFO = {
@@ -50,7 +40,7 @@ GimpPlugInInfo PLUG_IN_INFO = {
     run
 };
 
-MAIN ()
+MAIN()
 
 // The query function
 void query()
@@ -128,12 +118,10 @@ void run(const gchar * name,
 {
     // Create the return value.
     static GimpParam return_values[2];
-    GimpExportReturn export;
-
     *nreturn_vals = 1;
     *return_vals  = return_values;
 
-    // The status
+    // Set the return value to success by default
     return_values[0].type          = GIMP_PDB_STATUS;
     return_values[0].data.d_status = GIMP_PDB_SUCCESS;
 
@@ -168,8 +156,9 @@ void run(const gchar * name,
     else if(!strcmp(name, SAVE_PROCEDURE))
     {
         gint32 image_id, drawable_id;
-        int status;
+        int status = 1;
         float quality;
+        GimpExportReturn export_ret;
 
         // Check to make sure all of the parameters were supplied
         if(nparams != 6)
@@ -183,18 +172,18 @@ void run(const gchar * name,
 
         // Try to export the image
         gimp_ui_init(BINARY_NAME, FALSE);
-        export = gimp_export_image(&image_id,
-                                   &drawable_id,
-                                   "WEBP",
-                                   GIMP_EXPORT_CAN_HANDLE_RGB);
+        export_ret = gimp_export_image(&image_id,
+                                       &drawable_id,
+                                       "WEBP",
+                                       GIMP_EXPORT_CAN_HANDLE_RGB);
 
-        switch(export)
+        switch(export_ret)
         {
             case GIMP_EXPORT_EXPORT:
             case GIMP_EXPORT_IGNORE:
 
                 // Now get the settings
-                if(!save_dialog(&quality))
+                if(!export_dialog(&quality))
                 {
                     return_values[0].data.d_status = GIMP_PDB_CANCEL;
                     return;
@@ -215,252 +204,4 @@ void run(const gchar * name,
     }
     else
         return_values[0].data.d_status = GIMP_PDB_CALLING_ERROR;
-}
-
-// Reads the file from disk
-int read_webp(const gchar * filename)
-{
-    FILE * file;
-    long int filesize;
-    void * data,
-         * image_data;
-    int width, height;
-    gint32 new_image_id,
-           new_layer_id;
-    GimpDrawable * drawable;
-    GimpPixelRgn rgn;
-
-
-    // Try to open the file
-    file = fopen(filename, "rb");
-    if(!file)
-        return -1;
-
-    // Get the file size
-    fseek(file, 0, SEEK_END);
-    filesize = ftell(file);
-    fseek(file, 0, SEEK_SET);
-
-    // Now prepare a buffer of that size
-    // and read the data.
-    data = malloc(filesize);
-    fread(data, filesize, 1, file);
-
-    // Close the file
-    fclose(file);
-
-    // Perform the load procedure and free the raw data.
-    image_data = WebPDecodeRGB(data, filesize, &width, &height);
-    free(data);
-
-    // Check to make sure that the load was successful
-    if(!image_data)
-        return -1;
-
-    // Now create the new RGBA image.
-    new_image_id = gimp_image_new(width, height, GIMP_RGB);
-
-    // Create the new layer
-    new_layer_id = gimp_layer_new(new_image_id,
-                                  "Background",
-                                  width, height,
-                                  GIMP_RGB_IMAGE,
-                                  100,
-                                  GIMP_NORMAL_MODE);
-
-    // Get the drawable for the layer
-    drawable = gimp_drawable_get(new_layer_id);
-
-    // Get a pixel region from the layer
-    gimp_pixel_rgn_init(&rgn,
-                        drawable,
-                        0, 0,
-                        width, height,
-                        TRUE, FALSE);
-
-    // Now FINALLY set the pixel data
-    gimp_pixel_rgn_set_rect(&rgn,
-                            image_data,
-                            0, 0,
-                            width, height);
-
-    // We're done with the drawable
-    gimp_drawable_flush(drawable);
-    gimp_drawable_detach(drawable);
-
-    // Free the image data
-    free((void *)image_data);
-
-    // Add the layer to the image
-    gimp_image_add_layer(new_image_id, new_layer_id, 0);
-
-    // Set the filename
-    gimp_image_set_filename(new_image_id, filename);
-
-    return new_image_id;
-}
-
-// Writes the file to disk
-int write_webp(const gchar * filename, gint drawable_id, float quality)
-{
-    GimpDrawable * drawable;
-    gint bpp;
-    GimpPixelRgn rgn;
-    long int data_size;
-    void * image_data;
-    size_t output_size;
-    uint8_t * raw_data;
-    FILE * file;
-
-    // Get the drawable
-    drawable = gimp_drawable_get(drawable_id);
-
-    // Get the BPP
-    bpp = gimp_drawable_bpp(drawable_id);
-
-    // Get a pixel region from the layer
-    gimp_pixel_rgn_init(&rgn,
-                        drawable,
-                        0, 0,
-                        drawable->width,
-                        drawable->height,
-                        FALSE, FALSE);
-
-    // Determine the size of the array of image data to get
-    // and allocate it.
-    data_size = drawable->width * drawable->height * bpp;
-    image_data = malloc(data_size);
-
-    // Get the image data
-    gimp_pixel_rgn_get_rect(&rgn,
-                            (guchar *)image_data,
-                            0, 0,
-                            drawable->width,
-                            drawable->height);
-
-    // We have the image data, now encode it.
-    output_size = WebPEncodeRGB((const uint8_t *)image_data,
-                                drawable->width,
-                                drawable->height,
-                                drawable->width * 3,
-                                quality,
-                                &raw_data);
-
-    // Free the image data
-    free(image_data);
-
-    // Detach the drawable
-    gimp_drawable_detach(drawable);
-
-    // Make sure that the write was successful
-    if(output_size == FALSE)
-    {
-        free(raw_data);
-        return 0;
-    }
-
-    // Open the file
-    file = fopen(filename, "wb");
-    if(!file)
-    {
-        free(raw_data);
-        return 0;
-    }
-
-    // Write the data and be done with it.
-    fwrite(raw_data, output_size, 1, file);
-    free(raw_data);
-    fclose(file);
-
-    return 1;
-}
-
-// Response structure
-struct webp_data {
-    int       * response;
-    GtkObject * quality_scale;
-    float     * quality;
-};
-
-void on_response(GtkDialog * dialog,
-                 gint response_id,
-                 gpointer user_data)
-{
-    // Basically all we want to do is grab the value
-    // of the slider and store it in user_data.
-    struct webp_data * data   = user_data;
-    GtkHScale * hscale = GIMP_SCALE_ENTRY_SCALE(data->quality_scale);
-    gdouble returned_quality;
-
-    // Get the value
-    returned_quality = gtk_range_get_value(GTK_RANGE(hscale));
-    *(data->quality) = returned_quality;
-
-    // Quit the loop
-    gtk_main_quit();
-
-    if(response_id == GTK_RESPONSE_OK)
-        *(data->response) = 1;
-}
-
-// Displays the save options
-int save_dialog(float * quality)
-{
-    int response = 0;
-    struct webp_data data;
-    GtkWidget * dialog;
-    GtkWidget * vbox;
-    GtkWidget * label;
-    GtkWidget * table;
-    GtkObject * scale;
-
-    // Create the dialog
-    dialog = gimp_dialog_new("Save as WebP",
-                             BINARY_NAME,
-                             NULL, 0,
-                             NULL,
-                             SAVE_PROCEDURE,
-                             GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
-                             GTK_STOCK_SAVE,   GTK_RESPONSE_OK,
-                             NULL);
-
-    // Create the VBox
-    vbox = gtk_vbox_new(FALSE, 10);
-    gtk_container_set_border_width(GTK_CONTAINER(vbox), 10);
-    gtk_box_pack_start(GTK_BOX(GTK_DIALOG(dialog)->vbox),
-                       vbox, TRUE, TRUE, 2);
-    gtk_widget_show(vbox);
-
-    // Create the label
-    label = gtk_label_new("The options below allow you to customize\nthe WebP image that is created.");
-    gtk_box_pack_start(GTK_BOX(vbox), label, FALSE, FALSE, 2);
-    gtk_widget_show(label);
-
-    // Create the table
-    table = gtk_table_new(1, 3, FALSE);
-    gtk_box_pack_start(GTK_BOX(vbox), table, FALSE, FALSE, 6);
-    gtk_widget_show(table);
-
-    // Create the scale
-    scale = gimp_scale_entry_new(GTK_TABLE(table), 0, 0,
-                                 "Quality:",
-                                 150, 0,
-                                 90.0f, 0.0f, 100.0f, 1.0f, 10.0f,
-                                 0, TRUE, 0.0f, 0.0f,
-                                 "Quality for encoding the image",
-                                 NULL);
-
-    // Connect to the response signal
-    data.response      = &response;
-    data.quality_scale = scale;
-    data.quality       = quality;
-
-    g_signal_connect(dialog, "response", G_CALLBACK(on_response),   &data);
-    g_signal_connect(dialog, "destroy",  G_CALLBACK(gtk_main_quit), NULL);
-
-    // Now show and run the dialog.
-    gtk_widget_show(dialog);
-    gtk_main();
-
-    return response;
 }
